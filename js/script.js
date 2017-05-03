cc90a8d5c59f823ad72d68a1af5d133feb4a6397
Kristin Au <kristinau@Kristins-MacBook-Pro-3.local>
2017-05-03 16:28:39 -0700

    added dstyle project

--- js/script.js
+++ js/script.js
@@ -1,30 +1,16 @@
-$(window).load(function() {
+$(document).ready(function() {
 
   $("#comparison").twentytwenty();
   var header = new Headhesive('.navbar');
 
-  var bottomHeight = $('footer').outerHeight()+$('.project__end').outerHeight()+96+32;
-
-  $("#subnav").sticky({topSpacing:80, bottomSpacing: bottomHeight});
-
-  var subnav = $('#subnav');
-  $('a[href*=#]:not([href=#])').click(function(e) {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top - $('.navbar').height()
-            }, 1000);        
-            subnav.children().removeClass('subnav__link--active');
-            $(this).addClass('subnav__link--active');
-            return false;
-        }
-    }
-  });
+  $(".gif").hover(
+    function() {
+      var src = $(this).attr("src");
+      $(this).attr("src", src.replace(/\.png$/i, ".gif"));},
+    function() {
+      var src = $(this).attr("src");
+      $(this).attr("src", src.replace(/\.gif$/i, ".png"));}
+  );
 
 
 });
-
-    
